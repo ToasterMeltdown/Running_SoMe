856e2cd861492dd7b375f90140fc9416df57bc68
Andre Rorvik <andrero@uia.no>
2025-05-19 00:27:07 +0200

    added models

---
+++ Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+namespace Running_SoMe.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+namespace Running_SoMe.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
